5674d77efaae93fe36c9be87b6af4ea5e8c64eab
mujahid <mujahid.buet@gmail.com>
2022-01-11 23:36:48 +0600

    Fixed

--- docker/fusionauth/fusionauth-app-mysql/build.sh
+++ docker/fusionauth/fusionauth-app-mysql/build.sh
@@ -3,8 +3,8 @@
 DOCKER_HUB_USER=mujahid1005011
 FUSIONAUTH_VERSION=1.32.1
 
-docker build -t ${DOCKER_HUB_USER}/fusionauth-app-mysql:${FUSIONAUTH_VERSION} .
-docker build -t ${DOCKER_HUB_USER}/fusionauth-app-mysql:latest .
+docker build -t ${DOCKER_HUB_USER}/fusionauth-app-mysql:${FUSIONAUTH_VERSION} --network="host" .
+docker build -t ${DOCKER_HUB_USER}/fusionauth-app-mysql:latest --network="host" .
 
 docker login --username=${DOCKER_HUB_USER}
 
